d9b16d4e3f459e888fd9addaf8704b2a22e985b3
Andreglleal <andre.lemesleal@gmail.com>
2025-06-12 20:06:21 -0300

    Criação do DAO, Entidade e Constantes para o Room Database
    Este commit introduz as classes necessárias para a persistência de dados utilizando o Room:
    
    - **Contato:** Entidade que representa um contato com nome, sobrenome, idade e celular.
    - **ContatoDao:** Interface DAO com métodos para gravar, obter, atualizar e deletar contatos.
    - **Constantes:** Objeto que define os nomes da tabela e do banco de dados.

---
+++ app/src/main/java/com/trabalhoandroid/agendadecontatos/model/Contato.kt
@@ -0,0 +1,17 @@
+package com.trabalhoandroid.agendadecontatos.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.trabalhoandroid.agendadecontatos.constant.Constantes
+
+@Entity(tableName = Constantes.TABELA_CONTATOS)
+data class Contato (
+    @ColumnInfo(name = "nome") val name: String,
+    @ColumnInfo(name = "sobrenome") val sobrenome: String,
+    @ColumnInfo(name = "idade") val idade: String,
+    @ColumnInfo(name = "celular") val celular: String
+){
+    @PrimaryKey(autoGenerate = true)
+    var uid: Int = 0
+}
